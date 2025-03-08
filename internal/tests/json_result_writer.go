40378f1ba0ffe8ad3e15c4aabde408571ee7d72f
Patrick Foh <patrickfoh@gmail.com>
2025-03-08 18:03:40 +0000

    feat(output): Add customizable output file for test results

--- internal/tests/json_result_writer.go
+++ internal/tests/json_result_writer.go
@@ -10,10 +10,14 @@ type JSONResultWriter struct {
 	OutputPath string
 }
 
-func NewJSONResultWriter() *JSONResultWriter {
-	return &JSONResultWriter{
-		OutputPath: "test-results.json",
+func NewJSONResultWriter(outputPath string) *JSONResultWriter {
+	writer := &JSONResultWriter{}
+
+	if outputPath != "" {
+		writer.OutputPath = outputPath
 	}
+
+	return writer
 }
 
 // JSONResult is a serializable representation of test results
@@ -23,61 +27,61 @@ type JSONResult struct {
 }
 
 type JSONTestDefinition struct {
-	Name   string           `json:"name"`
-	Path   string           `json:"path"`
-	Suites []JSONTestSuite  `json:"suites"`
+	Name   string          `json:"name"`
+	Path   string          `json:"path"`
+	Suites []JSONTestSuite `json:"suites"`
 }
 
 type JSONTestSuite struct {
-	Name  string          `json:"name"`
-	Cases []JSONTestCase  `json:"cases"`
+	Name  string         `json:"name"`
+	Cases []JSONTestCase `json:"cases"`
 }
 
 type JSONTestCase struct {
-	Name          string   `json:"name"`
-	Passed        bool     `json:"passed"`
-	Timing        float64  `json:"timingMs"`
+	Name           string   `json:"name"`
+	Passed         bool     `json:"passed"`
+	Timing         float64  `json:"timingMs"`
 	FailureReasons []string `json:"failureReasons,omitempty"`
 }
 
 type JSONSummary struct {
-	TotalTestDefinitions int `json:"totalTestDefinitions"`
-	TotalSuites          int `json:"totalSuites"`
-	PassedSuites         int `json:"passedSuites"`
-	TotalCases           int `json:"totalCases"`
-	PassedCases          int `json:"passedCases"`
+	TotalTestDefinitions int     `json:"totalTestDefinitions"`
+	TotalSuites          int     `json:"totalSuites"`
+	PassedSuites         int     `json:"passedSuites"`
+	TotalCases           int     `json:"totalCases"`
+	PassedCases          int     `json:"passedCases"`
 	TotalTimeMs          float64 `json:"totalTimeMs"`
 }
 
 func (w *JSONResultWriter) Write(results map[string]TestDefinitionExecResult) {
 	jsonResult := JSONResult{
 		TestDefinitions: make([]JSONTestDefinition, 0, len(results)),
-		Summary: JSONSummary{},
+		Summary:         JSONSummary{},
 	}
-	
+
 	totalPassedSuites := 0
 	testSuiteCount := 0
 	testCaseCount := 0
 	passedTestCaseCount := 0
 	totalTiming := 0.0
-	
+
 	// Convert results to serializable format
 	for defName, defResult := range results {
 		jsonDef := JSONTestDefinition{
-			Name: defName,
-			Path: defResult.Path,
+			Name:   defName,
+			Path:   defResult.Path,
 			Suites: make([]JSONTestSuite, 0, len(defResult.Suites)),
 		}
-		
+
 		for suiteName, suiteResult := range defResult.Suites {
 			testSuiteCount++
 			jsonSuite := JSONTestSuite{
-				Name: suiteName,
+				Name:  suiteName,
 				Cases: make([]JSONTestCase, 0, len(suiteResult.Cases)),
 			}
-			
+
 			allCasesPassed := true
-			
+
 			for caseName, caseResult := range suiteResult.Cases {
 				testCaseCount++
 				if caseResult.Passed {
@@ -85,29 +89,29 @@ func (w *JSONResultWriter) Write(results map[string]TestDefinitionExecResult) {
 				} else {
 					allCasesPassed = false
 				}
-				
+
 				totalTiming += caseResult.Timing
-				
+
 				jsonCase := JSONTestCase{
 					Name:           caseName,
 					Passed:         caseResult.Passed,
 					Timing:         caseResult.Timing,
 					FailureReasons: caseResult.FailureReasons,
 				}
-				
+
 				jsonSuite.Cases = append(jsonSuite.Cases, jsonCase)
 			}
-			
+
 			if allCasesPassed {
 				totalPassedSuites++
 			}
-			
+
 			jsonDef.Suites = append(jsonDef.Suites, jsonSuite)
 		}
-		
+
 		jsonResult.TestDefinitions = append(jsonResult.TestDefinitions, jsonDef)
 	}
-	
+
 	// Fill in summary
 	jsonResult.Summary = JSONSummary{
 		TotalTestDefinitions: len(results),
@@ -117,14 +121,14 @@ func (w *JSONResultWriter) Write(results map[string]TestDefinitionExecResult) {
 		PassedCases:          passedTestCaseCount,
 		TotalTimeMs:          totalTiming,
 	}
-	
+
 	// Convert to JSON
 	jsonData, err := json.MarshalIndent(jsonResult, "", "  ")
 	if err != nil {
 		fmt.Printf("Error generating JSON output: %v\n", err)
 		return
 	}
-	
+
 	// Save to file if output path is set
 	if w.OutputPath != "" {
 		err = os.WriteFile(w.OutputPath, jsonData, 0644)
--- internal/tests/result_writer.go
+++ internal/tests/result_writer.go
@@ -4,14 +4,14 @@ type TestResultWriter interface {
 	Write(results map[string]TestDefinitionExecResult)
 }
 
-func NewResultWriter(output string) TestResultWriter {
-	switch output {
+func NewResultWriter(outputType string, outputFile string) TestResultWriter {
+	switch outputType {
 	case "text":
 		return NewTextResultWriter()
 	case "table":
 		return NewTableResultWriter()
 	case "json":
-		return NewJSONResultWriter()
+		return NewJSONResultWriter(outputFile)
 	default:
 		return NewTextResultWriter() // Default to text output
 	}
